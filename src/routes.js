e32e298e2217774825f063088393548848727f41
knowbee <knowbeeinc@gmail.com>
2021-02-27 13:54:34 +0200

    chat route

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Route, Switch, BrowserRouter } from "react-router-dom";
 import Register from "./components/Register/index";
 import Login from "./components/Login/index";
+import Chat from "./components/Chat/index";
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
       <Route path="/" exact={true} component={Register} />
       <Route path="/login" exact={true} component={Login} />
+      <Route path="/chat" component={Chat} />
     </Switch>
   </BrowserRouter>
 );
