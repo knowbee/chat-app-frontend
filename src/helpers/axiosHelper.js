af5c04050fc4b149c9b8e58e2e630159a0743d40
knowbee <knowbeeinc@gmail.com>
2021-02-27 12:01:48 +0200

    axios helper class

---
+++ src/helpers/axiosHelper.js
@@ -0,0 +1,68 @@
+import "dotenv/config";
+import axios from "axios";
+
+const APP_URL_BACKEND = process.env.REACT_APP_BACKEND_URL;
+
+let config = {
+  headers: {
+    "Content-Type": "application/json;charset=UTF-8",
+    "Access-Control-Allow-Origin": "*",
+  },
+};
+
+class AxiosHelper {
+  static post = (path, data, token) => {
+    config["headers"]["authorization"] = token;
+    return new Promise((resolve, reject) => {
+      return axios
+        .post(`${APP_URL_BACKEND}${path}`, data, config)
+        .then((response) => {
+          resolve(response);
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
+  };
+  static patch = (path, data, token) => {
+    config["headers"]["authorization"] = token;
+    return new Promise((resolve, reject) => {
+      return axios
+        .patch(`${APP_URL_BACKEND}${path}`, data, config)
+        .then((response) => {
+          resolve(response);
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
+  };
+  static delete = (path, data, token) => {
+    config["headers"]["authorization"] = token;
+    return new Promise((resolve, reject) => {
+      return axios
+        .delete(`${APP_URL_BACKEND}${path}`, config)
+        .then((response) => {
+          resolve(response);
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
+  };
+  static get = (path, data, token) => {
+    config["headers"]["authorization"] = token;
+    return new Promise((resolve, reject) => {
+      return axios
+        .get(`${APP_URL_BACKEND}${path}`, config)
+        .then((response) => {
+          resolve(response);
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
+  };
+}
+
+export default AxiosHelper;
