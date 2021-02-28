4eb3c9baa008d5dbacbadf88545d7ee0a08ef02f
knowbee <knowbeeinc@gmail.com>
2021-02-28 13:39:08 +0200

    protected routes

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,45 @@
 import React from "react";
-import { Route, Switch, BrowserRouter } from "react-router-dom";
+import { Route, Switch, BrowserRouter, Redirect } from "react-router-dom";
 import Register from "./components/Register/index";
 import Login from "./components/Login/index";
 import Chat from "./components/Chat/index";
-
-const Routes = () => (
+import { connect } from "react-redux";
+const Routes = ({ isLoggedIn }) => (
   <BrowserRouter>
     <Switch>
-      <Route path="/" exact={true} component={Register} />
-      <Route path="/login" exact={true} component={Login} />
-      <Route path="/chat" component={Chat} />
+      <Route
+        path="/"
+        exact={true}
+        render={(props) =>
+          !isLoggedIn ? <Register {...props} /> : <Redirect to="/chat" />
+        }
+      />
+      <Route
+        path="/login"
+        exact={true}
+        render={(props) =>
+          !isLoggedIn ? <Login {...props} /> : <Redirect to="/chat" />
+        }
+      />
+      <Route
+        exact={true}
+        path="/chat"
+        render={(props) =>
+          isLoggedIn ? <Chat {...props} /> : <Redirect to="/login" />
+        }
+      />
+      <Route
+        path="/*"
+        exact={true}
+        render={(props) =>
+          !isLoggedIn ? <Login {...props} /> : <Redirect to="/chat" />
+        }
+      />
     </Switch>
   </BrowserRouter>
 );
-export default Routes;
+
+export const mapStateToProps = ({ currentUser: { isLoggedIn } }) => ({
+  isLoggedIn,
+});
+export default connect(mapStateToProps)(Routes);
