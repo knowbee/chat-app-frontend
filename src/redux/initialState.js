559e615125a26fe86dfdcad6cbefad7feb41eba2
knowbee <knowbeeinc@gmail.com>
2021-02-28 14:53:17 +0200

    state

--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -18,6 +18,11 @@ module.exports = {
     password: "",
     errors: {},
   },
+  users: {
+    fetching: false,
+    chats: [],
+    errors: {},
+  },
   login: {
     submitting: false,
     credentials: {
